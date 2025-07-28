ceb60167150892a043184dc2eae5758aa38b08da
Hudson Lima Tavares <109999011+hltav@users.noreply.github.com>
2025-07-27 23:25:19 -0300

    Changed LoginForm.tsx

--- src/components/auth/login/LoginForm.tsx
+++ src/components/auth/login/LoginForm.tsx
@@ -26,7 +26,7 @@ export const LoginForm: React.FC = () => {
     setError(null);
 
     try {
-      const res = await fetch(`/api/auth/login`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include",
