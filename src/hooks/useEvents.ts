9abd7f46509f8d338137030688acb483a45f5485
Hudson Lima Tavares <109999011+hltav@users.noreply.github.com>
2025-09-03 21:06:20 -0300

    Create useEvents

---
+++ src/hooks/useEvents.ts
@@ -0,0 +1,45 @@
+import { getEvents } from "@/lib/api/events/methodsApiEvents";
+import { EventItem } from "@/modules/events/interfaces/EventItem";
+import { useState, useEffect } from "react";
+
+interface UseFetchEventsResult {
+  events: EventItem[];
+  loading: boolean;
+  error: Error | null;
+  refetch: () => void;
+}
+
+export const useFetchEvents = (): UseFetchEventsResult => {
+  const [events, setEvents] = useState<EventItem[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
+  const [shouldRefetch, setShouldRefetch] = useState<number>(0);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        const data = await getEvents();
+        setEvents(data);
+      } catch (err) {
+        console.error("Erro ao buscar eventos:", err);
+        setError(
+          err instanceof Error
+            ? err
+            : new Error("Ocorreu um erro desconhecido.")
+        );
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchEvents();
+  }, [shouldRefetch]);
+
+  const refetch = () => {
+    setShouldRefetch((prev) => prev + 1);
+  };
+
+  return { events, loading, error, refetch };
+};
