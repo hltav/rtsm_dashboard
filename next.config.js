adf876c26d3f1d7dacca8281354f3f26348f72cf
Hudson Lima Tavares <109999011+hltav@users.noreply.github.com>
2025-09-23 12:39:57 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://www.googletagmanager.com https://www.google-analytics.com;
+  script-src 'self' https://www.googletagmanager.com https://www.google-analytics.com https://apis.google.com;
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
   font-src 'self' https://fonts.gstatic.com;
   img-src 'self' data: https://www.google-analytics.com;
-  connect-src 'self' https://www.google-analytics.com;
+  connect-src 'self' https://www.google-analytics.com https://api.rtsportsmanager.com;
   frame-src 'self' https://www.youtube.com;
   frame-ancestors 'self';
 `;
@@ -17,9 +17,12 @@ const securityHeaders = [
   { key: 'Permissions-Policy', value: 'geolocation=(), camera=(), microphone=()' },
   { key: 'Content-Security-Policy', value: ContentSecurityPolicy.replace(/\n/g, '') },
   { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
+  { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+  { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
 ];
 
 const nextConfig = {
+  reactStrictMode: true,
   transpilePackages: [
     "@mui/material",
     "@mui/icons-material",
@@ -53,7 +56,6 @@ const nextConfig = {
       },
     ];
   },
-  reactStrictMode: true,
 };
 
 export default nextConfig;
