c9e2115ea6858cb67b201660d6d64e8dd02406be
Hudson Lima Tavares <109999011+hltav@users.noreply.github.com>
2025-09-27 23:33:03 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,39 +1,3 @@
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
-
-// export function middleware(request: NextRequest) {
-//     // Ignorar requisições RSC e internas
-//   if (
-//     request.nextUrl.searchParams.has('_rsc') ||
-//     request.headers.get('rsc') === '1' ||
-//     request.nextUrl.pathname.startsWith('/_next/') ||
-//     request.nextUrl.pathname.startsWith('/api/') ||
-//     request.nextUrl.pathname.includes('.')
-//   ) {
-//     console.log('Ignoring internal request');
-//     return NextResponse.next();
-//   }
-
-//   const accessToken = request.cookies.get("access_token");
-//   console.log('Access token present:', !!accessToken);
-
-//   if (!accessToken && request.nextUrl.pathname.startsWith('/dashboard')) {
-//     console.log('Redirecting to login');
-//     return NextResponse.redirect(new URL('/login', request.url));
-//   }
-
-//   if (accessToken && request.nextUrl.pathname === '/login') {
-//     console.log('Redirecting to dashboard');
-//     return NextResponse.redirect(new URL('/dashboard', request.url));
-//   }
-
-//   return NextResponse.next();
-// }
-
-// export const config = {
-//   matcher: ['/dashboard/:path*'],
-// };
-
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
@@ -47,7 +11,7 @@ export function middleware(request: NextRequest) {
     "Content-Security-Policy",
     `
       default-src 'self';
-      script-src 'self' 'nonce-${nonce}' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://apis.google.com;
+      script-src 'self' 'nonce-${nonce}' https://www.googletagmanager.com https://www.google-analytics.com https://apis.google.com;
       style-src 'self' 'nonce-${nonce}' https://fonts.googleapis.com;
       font-src 'self' https://fonts.gstatic.com;
       img-src 'self' data: https: https://www.google-analytics.com https://drive.google.com placehold.co;
