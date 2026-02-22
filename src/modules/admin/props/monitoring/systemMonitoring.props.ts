0f757ca1009cb7d591eea92a8d624a23a2901ac3
hltav <hudsonlimatavares@gmail.com>
2026-02-22 20:00:02 -0300

    Change SystemMonitoring File

--- src/modules/admin/props/monitoring/systemMonitoring.props.ts
+++ src/modules/admin/props/monitoring/systemMonitoring.props.ts
@@ -1,6 +1,6 @@
-// export interface SystemMonitoringHeaderProps {
-//   period: string;
-//   setPeriod: (value: string) => void;
-//   refreshData: () => void;
-//   isRefreshing: boolean;
-// }
+export interface SystemMonitoringHeaderProps {
+  period: string;
+  setPeriod: (value: string) => void;
+  refreshData: () => void;
+  isRefreshing: boolean;
+}
